b692f2dce36b4ab4b3b72ac3d16a973b2655256f
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2024-01-31 23:44:57 +0200

    Update locales.lua

--- tpz_doorlocks/locales.lua
+++ tpz_doorlocks/locales.lua
@@ -1,8 +1,9 @@
 Locales = {}
 
 Locales = {
-    ['LOCKED']   = "Locked",
-    ['UNLOCKED'] = "Unlocked",
+    ['LOCKED']                      = "Locked",
+    ['UNLOCKED']                    = "Unlocked",
 
-    ['LOCKPICKING_FAILED'] = "~e~The lockpick broke, progress has failed.",
+    ['LOCKPICKING_FAILED']          = "~e~The lockpick broke, progress has failed.",
+    ['PROPERTY_ROBBERY_BLIP_TITLE'] = "Property Robbery",
 }
