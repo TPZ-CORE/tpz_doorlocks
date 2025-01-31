d328fe5f3650ee1dc60c030e7d9b63370f234d4a
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-02-01 00:50:05 +0200

    Update fxmanifest.lua

--- tpz_doorlocks/fxmanifest.lua
+++ tpz_doorlocks/fxmanifest.lua
@@ -10,4 +10,10 @@ shared_scripts { 'config.lua', 'locales.lua', 'doorhashes.lua'   }
 client_scripts { 'client/*.lua' }
 server_scripts { 'server/*.lua' }
 
+dependencies {
+    'tpz_core',
+    'tpz_characters',
+    'tpz_inventory',
+}
+
 lua54 'yes'
